b435a854890e4edcd83f4fe024cde610587b0418
ajmal <ppttdqq@gmail.com>
2023-12-30 00:36:06 +0530

    Almost complete. Works left are quit prompt, game over score display

--- main.py
+++ main.py
@@ -63,26 +63,18 @@ def qmod():
 # create display window
 SCREEN_HEIGHT = 500
 SCREEN_WIDTH = 800
-global run
+
+
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Button Demo')
+pygame.display.set_caption('MATH QUIZ')
 
 # create font object
 pygame.font.init()
-font = pygame.font.Font('freesansbold.ttf', 46)
-# operations = [qsum, qdif, qpro, qdiv, qmod]
-# question = random.choice(operations)
-# returned_result = question()
-# quest_render = returned_result[0]
-# answer = returned_result[1]
-# opt_render = generate_options(answer)
-# correct_option = opt_render.index(answer) + 1
-# question_text = font.render(quest_render, True, ("White"))
-# option1_text = font.render(str(opt_render[0]), True, (0, 0, 0))
-# option2_text = font.render(str(opt_render[1]), True, (0, 0, 0))
-# option3_text = font.render(str(opt_render[2]), True, (0, 0, 0))
-# option4_text = font.render(str(opt_render[3]), True, (0, 0, 0))
+# font = pygame.font.Font('freesansbold.ttf', 46)
+score_and_life_font = pygame.font.Font('score-life.ttf',40)
+options_font = pygame.font.Font('score-life.ttf',60)
+question_font = pygame.font.Font('score-life.ttf',100)
 
 # load button images
 start_img = pygame.image.load('start_btn.png').convert_alpha()
@@ -98,7 +90,13 @@ option3 = button.Button(100, 400, option_img, 0.2)
 option4 = button.Button(420, 400, option_img, 0.2)
 
 
+score=0
+life=3
+screen_color = (200,200,180)
 def game_play():
+    global  score,life,isGameon,screen_color
+    score_text=f"Score : {score}"
+    life_text=f"Life: {life}"
     operations = [qsum, qdif, qpro, qdiv, qmod]
     question = random.choice(operations)
     returned_result = question()
@@ -106,51 +104,68 @@ def game_play():
     answer = returned_result[1]
     opt_render = generate_options(answer)
     correct_option = opt_render.index(answer) + 1
-    question_text = font.render(quest_render, True, ("White"))
-    option1_text = font.render(str(opt_render[0]), True, (0, 0, 0))
-    option2_text = font.render(str(opt_render[1]), True, (0, 0, 0))
-    option3_text = font.render(str(opt_render[2]), True, (0, 0, 0))
-    option4_text = font.render(str(opt_render[3]), True, (0, 0, 0))
-    global run
-    play = True
-    while play:
-        screen.fill((230, 155, 100))
-        screen.blit(question_text, (250, 230))
-        if option1.draw(screen):
-            opt_prs = 1
-        if option2.draw(screen):
-            opt_prs = 2
-        if option3.draw(screen):
-            opt_prs = 3
-        if option4.draw(screen):
-            opt_prs = 4
-        screen.blit(option1_text, (200, 310))
-        screen.blit(option2_text, (500, 310))
-        screen.blit(option3_text, (200, 420))
-        screen.blit(option4_text, (500, 420))
+    question_display = question_font.render(quest_render, True, ("Black"))
+    option1_display = options_font.render(str(opt_render[0]), True, (0, 0, 0))
+    option2_display = options_font.render(str(opt_render[1]), True, (0, 0, 0))
+    option3_display = options_font.render(str(opt_render[2]), True, (0, 0, 0))
+    option4_display = options_font.render(str(opt_render[3]), True, (0, 0, 0))
+    score_display=score_and_life_font.render(score_text, True, ("Black"))
+    life_display=score_and_life_font.render(life_text, True, ("Black"))
+
+
+    while isGameon and life>0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False
-                play = False
-        pygame.display.update()
+                isGameon = False
+
+        screen.fill(screen_color)
+        screen.blit(question_display, (235, 180))
+        screen.blit(life_display, (680,30 ))
+        screen.blit(score_display,(50,30))
+        opt_prs=0
+        if option1.draw(screen):
+            opt_prs = 1
+        elif option2.draw(screen):
+            opt_prs = 2
+        elif option3.draw(screen):
+            opt_prs = 3
+        elif option4.draw(screen):
+            opt_prs = 4
+        screen.blit(option1_display, (200, 310))
+        screen.blit(option2_display, (510, 310))
+        screen.blit(option3_display, (200, 410))
+        screen.blit(option4_display, (510, 410))
+
+        if opt_prs == correct_option:
+            score+=1
+            screen_color=(20,100,20)
+            game_play()
+            isGameon=False
+        elif opt_prs!=0:
+            life-=1
+            screen_color = (200, 30, 30)
+            game_play()
+            isGameon = False
+
+        pygame.display.flip()
 
 
 # game loop
-run = True
-while run:
+isGameon=True
+while isGameon:
 
     screen.fill((202, 228, 241))
 
     if start_button.draw(screen):
         game_play()
     if exit_button.draw(screen):
-        run = False
+        isGameon = False
 
     # event handler
     for event in pygame.event.get():
         # quit game
         if event.type == pygame.QUIT:
-            run = False
+            isGameon = False
 
     pygame.display.update()
 
